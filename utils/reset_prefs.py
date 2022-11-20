51f842e8fccf580f4c3c45f88e4857a3ee7c8bae
Sree Gopinath <sree.gopinath@nyu.edu>
2022-11-19 19:49:57 -0800

    Save interests before deleting

--- utils/reset_prefs.py
+++ utils/reset_prefs.py
@@ -4,13 +4,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-def reset_prefs():
+def reset_prefs(uname, pword):
     driver = webdriver.Chrome()
     try:
         driver.get('https://myadcenter.google.com/controls')
 
         username = driver.find_element(By.NAME, "identifier")
-        username.send_keys("")  # username here
+        username.send_keys(uname)  # username here
         driver.find_element(By.ID, "identifierNext").click()
 
         nextButton = WebDriverWait(driver, 30).until(
@@ -19,23 +19,47 @@ def reset_prefs():
         password = WebDriverWait(driver, 30).until(
             EC.presence_of_element_located((By.NAME, "Passwd"))
         )
-        password.send_keys("")  # password here
+        password.send_keys(pword)  # password here
         driver.execute_script("arguments[0].click();", nextButton)
+
+        my_ads = WebDriverWait(driver, 30).until(
+                 EC.presence_of_element_located((By.XPATH, "/html/body/c-wiz/div/div[2]/gm-coplanar-drawer/div/div/div/ul/li[1]/span[3]/span"))
+        )
+        driver.execute_script("arguments[0].click();", my_ads)
+
+        see_all_topics = WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "/html/body/c-wiz/div/div[2]/div/c-wiz/div/div[2]/c-wiz[1]/div/div[1]/div/div/a"))
+        )
+        driver.execute_script("arguments[0].click();", see_all_topics)
+
+        WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "/html/body/c-wiz/div/div[2]/div/c-wiz/div/div[2]/div[2]/span/div/c-wiz/div/ul"))
+        )
+        element_list = driver.find_element(By.XPATH, "/html/body/c-wiz/div/div[2]/div/c-wiz/div/div[2]/div[2]/span/div/c-wiz/div/ul")
+        items = element_list.find_elements(By.TAG_NAME, "li")
+
+        f = open(uname + "_interests.txt", "a+")
+        for item in items:
+            text = item.text
+            f.write(text + "\n")
+        f.close()
 
         button = WebDriverWait(driver, 30).until(
             EC.presence_of_element_located((By.XPATH, "/html/body/c-wiz/div/div[2]/div/c-wiz/div/c-wiz/div/div/div[1]/c-wiz/div/gm3-tonal-button"))
         )
         driver.execute_script("arguments[0].click();", button)
 
-        turnOffButton = WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/div[7]/div[2]/div/div[2]/div[2]/div/gm3-text-button[2]/button/span[1]"))
+        turn_off_button = WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div[8]/div[2]/div/div[2]/div[2]/div/gm3-text-button[2]/button/span[1]"))
         )
-        driver.execute_script("arguments[0].click();", turnOffButton)
+        driver.execute_script("arguments[0].click();", turn_off_button)
 
-        gotItButton = WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/div[7]/div[2]/div/div[2]/gm3-text-button/button/span[1]"))
+        got_it_button = WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div[8]/div[2]/div/div[2]/gm3-text-button/button/span[1]"))
         )
-        driver.execute_script("arguments[0].click();", gotItButton)
+        driver.execute_script("arguments[0].click();", got_it_button)
     except Exception as e:
         print("Exception ", e)
     finally:
@@ -44,4 +68,9 @@ def reset_prefs():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    driver = reset_prefs()
+    f = open("creds.txt", "r")
+    lines = f.readlines()
+    for line in lines:
+        creds = line.split(",")
+        driver = reset_prefs(creds[0], creds[1])
+        driver.close()
